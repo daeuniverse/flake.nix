61d53d806f985ae039094976807eac2c92efe676
oluceps <i@oluceps.uk>
2023-07-27 01:32:15 +0800

    chore: streamline automated workflow

--- dae/package.nix
+++ dae/package.nix
@@ -1,44 +1,47 @@
 { clang
 , fetchFromGitHub
 , buildGoModule
+, lib
 }:
-buildGoModule rec {
-  pname = "dae";
-  version = "0.2.2";
+buildGoModule (
+  let hashes = lib.strings.splitString "\n" (builtins.readFile ./HASH); in rec {
+    pname = "dae";
+    version = "0.2.2";
 
-  src = fetchFromGitHub {
-    owner = "daeuniverse";
-    repo = pname;
-    rev = "v${version}";
-    sha256 = "sha256-PAdhIhX2hXCgg3NAZR0k8d1I/qQ8Cs8bNT6s6tZ5t4E=";
-    fetchSubmodules = true;
-  };
+    src = fetchFromGitHub {
+      owner = "daeuniverse";
+      repo = pname;
+      rev = "v${version}";
+      hash = lib.elemAt hashes 0;
+      fetchSubmodules = true;
+    };
 
-  vendorHash = "sha256-DCEvE2ES7GwiXKyD7tRlykqiIaKdmo7TczutPeGurKw=";
+    vendorHash = lib.elemAt hashes 1;
 
-  proxyVendor = true;
+    proxyVendor = true;
 
-  nativeBuildInputs = [ clang ];
+    nativeBuildInputs = [ clang ];
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-X github.com/daeuniverse/dae/cmd.Version=${version}"
-    "-X github.com/daeuniverse/dae/common/consts.MaxMatchSetLen_=64"
-  ];
+    ldflags = [
+      "-s"
+      "-w"
+      "-X github.com/daeuniverse/dae/cmd.Version=${version}"
+      "-X github.com/daeuniverse/dae/common/consts.MaxMatchSetLen_=64"
+    ];
 
-  preBuild = ''
-    make CFLAGS="-D__REMOVE_BPF_PRINTK -fno-stack-protector" \
-    NOSTRIP=y \
-    ebpf
-  '';
+    preBuild = ''
+      make CFLAGS="-D__REMOVE_BPF_PRINTK -fno-stack-protector" \
+      NOSTRIP=y \
+      ebpf
+    '';
 
-  # network required
-  doCheck = false;
+    # network required
+    doCheck = false;
 
-  postInstall = ''
-    install -Dm444 install/dae.service $out/lib/systemd/system/dae.service
-    substituteInPlace $out/lib/systemd/system/dae.service \
-      --replace /usr/bin/dae $out/bin/dae
-  '';
-}
+    postInstall = ''
+      install -Dm444 install/dae.service $out/lib/systemd/system/dae.service
+      substituteInPlace $out/lib/systemd/system/dae.service \
+        --replace /usr/bin/dae $out/bin/dae
+    '';
+  }
+)
