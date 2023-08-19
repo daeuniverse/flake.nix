e3c14efabd51904218dd24b5941c69698c26db4e
oluceps <i@oluceps.uk>
2023-08-18 19:15:46 -0600

    feat: automated update (#4)
    * chore: add dae source into input
    
    * ci: auto update source lock
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'dae-src':
        'git+https://github.com/daeuniverse/dae?ref=refs/heads/main&rev=afae72e4b94a9b4267c60494b156ed0465084e14' (2023-07-28)
      → 'git+https://github.com/daeuniverse/dae?ref=refs/heads/main&rev=e4a95aade06883d075e236ccae0f74a11f7cbd48' (2023-07-29)
    
    * chore: update vendorHash
    
    * feat: auto update package
    
    chore: update dae package
    
    chore: clean code

--- dae/package.nix
+++ dae/package.nix
@@ -1,47 +1,44 @@
 { clang
 , fetchFromGitHub
 , buildGoModule
-, lib
 }:
-buildGoModule (
-  let dyn = lib.strings.splitString "\n" (builtins.readFile ./dynamic_info); in rec {
-    pname = "dae";
-    version = with lib; substring 0 8 (elemAt dyn 0);
+buildGoModule rec {
+  pname = "dae";
+  version = "unstable-2023-08-14";
 
-    src = fetchFromGitHub {
-      owner = "daeuniverse";
-      repo = pname;
-      rev = lib.elemAt dyn 0;
-      hash = lib.elemAt dyn 1;
-      fetchSubmodules = true;
-    };
+  src = fetchFromGitHub {
+    owner = "daeuniverse";
+    repo = pname;
+    rev = "8bbfd691a7034b6007600ed65547cb84f446d387";
+    sha256 = "sha256-WiJqhXYehuUCLEuVbsQkmTntuH1srtePtZgYBSTbxiw=";
+    fetchSubmodules = true;
+  };
 
-    vendorHash = lib.elemAt dyn 2;
+  vendorHash = "sha256-fb4PEMhV8+5zaRJyl+nYi2BHcOUDUVAwxce2xaRt5JA=";
 
-    proxyVendor = true;
+  proxyVendor = true;
 
-    nativeBuildInputs = [ clang ];
+  nativeBuildInputs = [ clang ];
 
-    ldflags = [
-      "-s"
-      "-w"
-      "-X github.com/daeuniverse/dae/cmd.Version=${version}"
-      "-X github.com/daeuniverse/dae/common/consts.MaxMatchSetLen_=64"
-    ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/daeuniverse/dae/cmd.Version=${version}"
+    "-X github.com/daeuniverse/dae/common/consts.MaxMatchSetLen_=64"
+  ];
 
-    preBuild = ''
-      make CFLAGS="-D__REMOVE_BPF_PRINTK -fno-stack-protector" \
-      NOSTRIP=y \
-      ebpf
-    '';
+  preBuild = ''
+    make CFLAGS="-D__REMOVE_BPF_PRINTK -fno-stack-protector" \
+    NOSTRIP=y \
+    ebpf
+  '';
 
-    # network required
-    doCheck = false;
+  # network required
+  doCheck = false;
 
-    postInstall = ''
-      install -Dm444 install/dae.service $out/lib/systemd/system/dae.service
-      substituteInPlace $out/lib/systemd/system/dae.service \
-        --replace /usr/bin/dae $out/bin/dae
-    '';
-  }
-)
+  postInstall = ''
+    install -Dm444 install/dae.service $out/lib/systemd/system/dae.service
+    substituteInPlace $out/lib/systemd/system/dae.service \
+      --replace /usr/bin/dae $out/bin/dae
+  '';
+}
